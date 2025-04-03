7a10c55a84c9bb518bf87d2af88660e203433c03
zmx2321 <zmx2321@163.com>
2025-04-03 15:54:52 +0800

    fix: init

--- deploy.sh
+++ deploy.sh
@@ -4,17 +4,17 @@
 set -e
 
 # 生成静态文件
-yarn build:pro
+# yarn build:pro
 
 # 进入生成的文件夹
-cd vvue3_admin_test
+cd vue3_admin_test
 
-# git pull
+git pull
 git init
 git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io  填写你刚刚创建的仓库地址
-git push -f git@github.com:zmx2321/vvue3_admin_test.git main:gh-pages
+git push -f git@github.com:zmx2321/vue3_admin_test.git main:gh-pages
 
 cd -
